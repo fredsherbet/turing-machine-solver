517c0d182f01d34058a8aae20194d6c9a0d100e5
Matthew Russell <matt@fredsherbet.com>
2025-01-05 18:33:16 +0000

    Futher refactoring

--- turing.py
+++ turing.py
@@ -3,9 +3,40 @@ import argparse
 import sys
 from rules import card_rules
 
-# Load up the rules
-# For each combination of possible rules, determine whether there's one and only one answer
-# And also are there any rules that are redundant?
+def solve_for_cards(cards):
+    """Analyse the cards, and figure out what possible rules and answers there are"""
+    # Turing Machine relies on the following facts:
+    #   * There is only one solution (i.e. the rules must have only a single code that fits)
+    #   * Every card is needed to determine the solution (i.e. no card is redundant)
+    #
+    # We'll use the first fact to get a list of candidates, then filter it down by eliminating
+    # candidates that have redundant cards.
+
+    # Do we know all the rules needed?
+    all_good = True
+    for c in cards:
+        try:
+            card_rules[c]
+        except KeyError:
+            all_good = False
+            print(f"I do not know the rules for card {c}")
+    if not all_good:
+        return
+    
+    #print("Rule sets that provide a single solution:")
+    options = list(find_rules_that_produce_single_result(cards))
+    #short_print_rules(options)
+
+    # Can we eliminate some, due to some rules being redundant?
+    options = [o for o in options if not o.has_redundant_card()]
+    print(f"\nIgnoring rules that contain redundant rules (got {len(options)}):")
+    short_print_rules(options)
+
+    if len(options) > 1:
+        # We don't have a single solution. Can we help the player decide what to do?
+        print("\nThat means:\n")
+        print_card_information(cards, options)
+
 
 class Guess:
     def __init__(self, b, y, p):
@@ -24,10 +55,20 @@ class Guess:
         yield self.y
         yield self.p
 
+
 class Option:
     def __init__(self, solution, rules):
         self.solution = solution
         self.rules = rules
+
+    def has_redundant_card(self):
+        for r in combinations(self.rules, len(self.rules)-1):
+            if find_unique_solution(r):
+                # Found a unique solution, despite ignoring a rule
+                #print(f"{g}: {rule_set} IS BAD because subset {r} gives a unique solution")
+                return True
+        return False
+        
 
 def all_possible_guesses():
     for b,y,p in product(range(1, 6), range(1, 6), range(1, 6)):
@@ -45,7 +86,7 @@ def find_unique_solution(rule_set):
         return good_guess
 
 def possible_solutions_for_rule(rule, options):
-    return [o.solution for o in options if rule in o.rules]
+    return (o.solution for o in options if rule in o.rules)
 
 def short_print_rules(options):
     print("\n".join(f"{o.solution}: {o.rules}" for o in options))
@@ -57,8 +98,8 @@ def print_card_information(cards, options):
                 print(f"* Card {card} is definitely `{rule}`")
                 break
         else:
-            print(f"* Card {card} could be \n{"\n".join(
-                f"    * `{str(rule)}` ({", ".join(str(s) for s in possible_solutions_for_rule(rule, options))})" 
+            print(f"* Card {card} could be {"".join(
+                f"\n    * `{rule}` ({", ".join(str(s) for s in possible_solutions_for_rule(rule, options))})" 
                 for rule in card_rules[card] if any(rule in o.rules for o in options))
             }")
 
@@ -67,45 +108,6 @@ def find_rules_that_produce_single_result(cards):
         g = find_unique_solution(rule_set)
         if g:
             yield Option(g, rule_set)
-
-def eliminate_sets_with_redundant_cards(options):
-    bad_rules = set()
-    for option in options:
-        rule_set = option.rules
-        for r in combinations(rule_set, len(rule_set)-1):
-            if find_unique_solution(r):
-                # Found a unique solution, despite ignoring a rule
-                #print(f"{g}: {rule_set} IS BAD because subset {r} gives a unique solution")
-                bad_rules.add(rule_set)
-                break
-    return (o for o in options if o.rules not in bad_rules)
-
-def solve_for_cards(cards):
-
-    # Have we got all the rules needed?
-    all_good = True
-    for c in cards:
-        try:
-            card_rules[c]
-        except KeyError:
-            all_good = False
-            print(f"I do not know the rules for card {c}")
-    if not all_good:
-        return
-    
-    #print("Rule sets that provide a single solution:")
-    options = list(find_rules_that_produce_single_result(cards))
-    #short_print_rules(options)
-
-    # Can we eliminate some, due to some rules being redundant?
-    options = list(eliminate_sets_with_redundant_cards(options))
-    print(f"\nIgnoring rules that contain redundant rules (got {len(options)}):")
-    short_print_rules(options)
-
-    if len(options) > 1:
-        # We don't have a single solution. Can we help the player decide what to do?
-        print("\nThat means:\n")
-        print_card_information(cards, options)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="A Turing Machine game analyser")
